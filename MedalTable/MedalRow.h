42f52df38d61f0a5218d17af155c22962626dd98
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-07-18 11:24:08 +0700

    Commit 2
    Add constructors

--- MedalTable/MedalRow.h
+++ MedalTable/MedalRow.h
@@ -1,6 +1,26 @@
 ﻿#pragma once
+#include<iostream>
 // класс MedalRow
 class MedalRow
 {
+private:
+	char country[4];
+	int medals[3];
+public:
+// static поля класса для доступа к медалям
+	static const int GOLD{ 0 };
+	static const int SILVER{ 1 };
+	static const int BRONZE{ 2 };
+// конструктор с параметрами
+	MedalRow(const char* countryP, const int* medalsP)
+	{
+		strcpy_s(country, 4, countryP ? countryP : "NON");
+		for (int i{ 0 }; i < 3; ++i)
+		{
+			medals[i] = medalsP ? medalsP[i] : 0;
+		}
+	}
+// конструктор по умолчанию
+	MedalRow() : MedalRow(nullptr, nullptr) {}
 };
 
