d58b6f700ecc29d1ec8895afb54e0c1491f9bb6c
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-07-18 14:53:45 +0700

    Commit 6
    Add constructors and desctructor in MedalsTable
    Add private metod findCountry

--- MedalTable/MedalsTable.cpp
+++ MedalTable/MedalsTable.cpp
@@ -1,1 +1,14 @@
 ﻿#include "MedalsTable.h"
+#include<iostream>
+
+using namespace std;
+// проверка есть ли уже такая страна в объекте
+int MedalsTable::findCountry(const char* country) const {
+	for (int i{ 0 }; i < size; ++i)	{
+		if (strcmp(medalRows[i].getCountry(),
+			country) == 0)	{
+			return i;
+		}
+	}
+	return -1;
+}
--- MedalTable/MedalsTable.h
+++ MedalTable/MedalsTable.h
@@ -3,5 +3,22 @@
 // класс MedalsTable
 class MedalsTable
 {
+private:
+	int size;
+	MedalRow * medalRows;
+	int findCountry(const char* country) const;
+public:
+// конструктор с параметрами
+	MedalsTable(int szP) : size{ szP } {
+		if (szP == 0)
+			this->medalRows = nullptr;
+		else
+			this->medalRows = new MedalRow[szP];
+	}
+// конструктор по умолчанию
+	MedalsTable() : MedalsTable(0) {};
+	~MedalsTable() {
+		delete[]this->medalRows
+	}
 };
 
